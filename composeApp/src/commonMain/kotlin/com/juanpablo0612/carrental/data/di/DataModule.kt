a6b8ae625ad623aa9194f647b499d1fcb47610f8
JuanPablo0612 <gallegojuanpablo39@gmail.com>
2025-11-16 16:32:55 -0500

    feat: update JVM target to 21 and configure Koin for VehiclesRemoteDataSource and VehiclesRepositoryImpl

--- composeApp/src/commonMain/kotlin/com/juanpablo0612/carrental/data/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/juanpablo0612/carrental/data/di/DataModule.kt
@@ -2,9 +2,16 @@ package com.juanpablo0612.carrental.data.di
 
 import com.juanpablo0612.carrental.data.vehicles.FakeVehiclesRepository
 import com.juanpablo0612.carrental.data.vehicles.VehiclesRepository
+import com.juanpablo0612.carrental.data.vehicles.VehiclesRepositoryImpl
+import com.juanpablo0612.carrental.data.vehicles.remote.VehiclesRemoteDataSource
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.firestore.firestore
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dataModule = module {
-    singleOf<VehiclesRepository>(::FakeVehiclesRepository)
+    single { Firebase.firestore }
+    singleOf(::VehiclesRemoteDataSource)
+    singleOf(::VehiclesRepositoryImpl) bind VehiclesRepository::class
 }
